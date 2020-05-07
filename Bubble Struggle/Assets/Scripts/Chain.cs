f4627b5ac86eca58486fc6eae8a911bb3d93857b
Mohammad Al-Haddad <MohammadxCoder@icloud.com>
2020-05-07 06:42:03 +0300

    Changed Collision Layers Added Chain

---
+++ Bubble Struggle/Assets/Scripts/Chain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    public static bool IsFired;
+    public float chainSpeed = 2f;
+
+    private Transform _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsFired = false;
+        _player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            IsFired = true;
+        }
+
+        if (IsFired)
+        {
+            transform.localScale = transform.localScale + Vector3.up * (chainSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = _player.position;
+            transform.localScale = new Vector3(1f,0,1f);
+        }
+    }
+}
---
+++ Bubble Struggle/Assets/Scripts/ChainCollision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print($"{other.gameObject.name} Triggered it" );
+        Chain.IsFired = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        print($"{other.gameObject.name} Entered it");
+    }
+}
