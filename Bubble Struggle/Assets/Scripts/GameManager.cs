fa26a93803a9b916356c51722d11f1b6b0137786
Mohammad Al-Haddad <MohammadxCoder@icloud.com>
2020-05-08 08:15:42 +0300

    Added MainMenu

---
+++ Bubble Struggle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
